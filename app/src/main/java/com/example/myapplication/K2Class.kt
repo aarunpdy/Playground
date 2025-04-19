97a995830509fe200daf77e36e218e0afc6fa515
arun <aarunpdy@gmail.com>
2025-04-19 21:47:37 +0530

    Initial project settings

---
+++ app/src/main/java/com/example/myapplication/K2Class.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * @author arun
+ * Created on 14,Dec 2024
+ * https://medium.com/@omarsahl/kotlin-language-features-from-kotlinconf-2024-whats-new-and-what-s-next-a4668eae5e9d
+ */
+
+class K2Class {
+
+    val title:StateFlow<String>
+        field = MutableStateFlow<String>("")
+
+    fun myFun() {
+        title.value
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/theme/MyContent.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.runtime.Composable
+import com.example.myapplication.MainActivity
+
+/**
+ * @author arun
+ * Created on 14,Dec 2024
+ */
+@Composable
+fun MyContent() {
+
+}
