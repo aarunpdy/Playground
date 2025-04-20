a3e9463ddb75d158a9f637eb79f572723486ba91
arun <aarunpdy@gmail.com>
2025-04-20 09:03:15 +0530

    Basic Android Project setup

---
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.playground.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+val k2 = K2Class()
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Jetpack Compose")
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text("Click Me")
+        }
+    }
+
+    if (3 isGreaterThan 5) {
+
+    }
+}
+
+/**
+ * My custom infix function
+ */
+infix fun Int.isGreaterThan(value: Int): Boolean {
+    return this > value
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/playground/ui/theme/MyContent.kt
@@ -0,0 +1,12 @@
+package com.example.playground.ui.theme
+
+import androidx.compose.runtime.Composable
+
+/**
+ * @author arun
+ * Created on 14,Dec 2024
+ */
+@Composable
+fun MyContent() {
+
+}
